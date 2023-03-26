b84e6a29a1d49b24695192f9d0023247463ddf48
Feldmarija <marija.feldmane@inbox.lv>
2023-03-26 19:56:38 +0300

    [classroom_15032023]: Add Classroom Progress

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,32 @@
+import classroom.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+
+    Calculator casio = new Calculator();
+    @Test
+    public void testSum () {
+
+     //   System.out.println(casio.sum(5,5));
+     // int sumNumber = casio.sum(5,5);
+     //   System.out.println(sumNumber);
+        Assert.assertEquals(casio.sum(10,50), 60);
+    }
+
+    @Test
+    public void testDivide () {
+        Assert.assertEquals(casio.divide(50,10),5);
+    }
+
+    @Test
+    public void testMultiply () {
+        Assert.assertEquals(casio.multiply(50,10), 500);
+    }
+
+    @Test
+    public void testSubtract () {
+        Assert.assertEquals(casio.subtracts(50,10), 40);
+    }
+
+}
