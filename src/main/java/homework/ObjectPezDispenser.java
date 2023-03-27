ab891a3715649dc418ceaaadd63ec5e36c41c853
Feldmarija <marija.feldmane@inbox.lv>
2023-03-27 20:59:04 +0300

    [homework_19032023]: Pez dispenser homework

---
+++ src/main/java/homework/ObjectPezDispenser.java
@@ -0,0 +1,41 @@
+package homework;
+
+public class ObjectPezDispenser {
+    public static void main(String[] args) {
+        PezDispenser dispenser = new PezDispenser("Bob", "Silver", "Minions", 6);
+        System.out.println();
+
+        System.out.println(dispenser);
+        System.out.println();
+
+        System.out.println("Pez dispenser " + dispenser.name + " currently contains " + dispenser.pezCount + " candies");
+        System.out.println();
+
+        dispenser.load();
+        System.out.println("Pez dispenser " + dispenser.name + " currently contains " + dispenser.pezCount + " candies");
+        System.out.println();
+
+        dispenser.dispense();
+        System.out.println("Pez dispenser " + dispenser.name + " currently contains " + dispenser.pezCount + " candies");
+        System.out.println();
+
+        dispenser.dispenseX(2);
+        System.out.println("Pez dispenser " + dispenser.name + " currently contains " + dispenser.pezCount + " candies");
+        System.out.println();
+
+        dispenser.loadX(5);
+        System.out.println("Pez dispenser " + dispenser.name + " currently contains " + dispenser.pezCount + " candies");
+        System.out.println();
+
+        dispenser.loadMissingCandies();
+        System.out.println("Pez dispenser " + dispenser.name + " currently contains " + dispenser.pezCount + " candies");
+        System.out.println();
+
+
+
+    }
+
+
+
+
+}
